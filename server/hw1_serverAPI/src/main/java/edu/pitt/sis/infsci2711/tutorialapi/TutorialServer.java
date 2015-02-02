77a04ec074441d9d76381be1b083ed507bfbad32
wenzhang61 <wez61@pitt.edu>
2015-02-02 16:12:43 -0500

    server

---
+++ server/hw1_serverAPI/src/main/java/edu/pitt/sis/infsci2711/tutorialapi/TutorialServer.java
@@ -0,0 +1,13 @@
+package edu.pitt.sis.infsci2711.tutorialapi;
+
+import edu.pitt.sis.infsci2711.multidbs.utils.JerseyJettyServer;
+
+public class TutorialServer {
+	
+	public static void main(final String[] args) throws Exception {
+		JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.tutorial.rest");
+		server.start();
+
+	
+	}
+}
